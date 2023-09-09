eb8cb0ad049534b7523b51cde20569644fe275d0
Hernandes <hernandesvinicius2005@gmail.com>
2023-09-09 19:29:34 -0300

    UI/UX: Regex aplicado na busca

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,15 +7,18 @@ export default function Search() {
   const [data, setData] = useState(null);
   const [city, setCity] = useState('');
 
-  //O UseEffect sempre será chamado quando ocorrer alguma mudança na variável 'city'.
+  // O UseEffect sempre será chamado quando ocorrer alguma mudança na variável 'city'.
   useEffect(() => {
-    const fetchData = async () => { //Função assíncrona para evitar possíveis erros de variáveis 'indefinidas'. Salvo em uma constante.
+    // Função assíncrona para evitar possíveis erros de variáveis 'indefinidas'.
+    const fetchData = async () => { 
       try {
-        const res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
+        let regexCity = city.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
+        
+        const res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${regexCity}`);
         setData(res.data); //Salva o estado da variável data.
       } catch (err) {
         console.error('Erro ao buscar dados da API:', err);
-        // Aqui você pode definir um estado de erro e mostrar uma mensagem de erro ao usuário.
+        // Aqui você pode definir um estado de erro e mocityar uma mensagem de erro ao usuário.
       }
     };
 
@@ -30,10 +33,10 @@ export default function Search() {
     <div className="container">
       <input placeholder='Digite sua cidade aqui!' value={city} onChange={handleCityChange}/>
       <button>Buscar</button>
-      {data ? ( <Bloc data={data} /> ) : (
-        // Adicione uma mensagem de carregamento ou erro aqui.
-        <p>Tente fazer uma pesquisa ou aguarde alguns segundos...</p>
-      )}
+      {city === "" ? (<p>Tente realizar alguma busca</p>) : (
+        data ? ( <Bloc data={data} /> ) : (
+          <p>Por favor aguarde alguns segundos...</p>// Adicione uma mensagem de carregamento ou erro aqui.
+        ))}
     </div>
   );
 }
